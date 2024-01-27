9cc96584b44e352ca1f6ed6fdecf5034061e2032
Abhishek Santra <abhisheksantra28@gmail.com>
2024-01-27 12:47:13 +0530

    Fix import statement

--- Server/src/controllers/user.controller.js
+++ Server/src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import { asyncHander } from "../utils/asycHandler.js";
-import ApiError from "../utils/ApiError.js";
+import {ApiError} from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import {
   uploadOnCloudinary,
