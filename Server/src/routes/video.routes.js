c84719f019776be35668ceb96125f4d725db3f51
Abhishek Santra <abhisheksantra28@gmail.com>
2024-02-04 13:05:19 +0530

    Add publish-video endpoint

--- Server/src/routes/video.routes.js
+++ Server/src/routes/video.routes.js
@@ -1,3 +1,23 @@
 import { Router } from "express";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { publishAVideo } from "../controllers/video.controller.js";
 
-const router = Router()
+const router = Router();
+
+router.route("/publish-video").post(
+  verifyJWT,
+  upload.fields([
+    {
+      name: "videFile",
+      maxCount: 1,
+    },
+    {
+      name: "thumbnail",
+      maxCount: 1,
+    },
+  ]),
+  publishAVideo
+);
+
+export default router;
